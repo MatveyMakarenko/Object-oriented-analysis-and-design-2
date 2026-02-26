805057ea04d6bf93a16b68e68831494af0d6470b
MatveyMakarenko <148697980+MatveyMakarenko@users.noreply.github.com>
2026-02-26 22:00:34 +0700

    Update TechLight.cs

--- lab01/vs/WindowsFormsApp1/WindowsFormsApp1/Devices/TechLight.cs
+++ lab01/vs/WindowsFormsApp1/WindowsFormsApp1/Devices/TechLight.cs
@@ -4,30 +4,31 @@ namespace SmartHomeWithoutPattern.Devices
 {
     public class TechLight
     {
-        private bool _isOn;
-        private int _brightness;
+        private bool isOn;
+        private int brightness;
 
         public void TurnOn()
         {
-            _isOn = true;
+            isOn = true;
             Console.WriteLine("[TechPro] Свет включен");
         }
 
         public void TurnOff()
         {
-            _isOn = false;
+            isOn = false;
             Console.WriteLine("[TechPro] Свет выключен");
         }
 
         public void SetBrightness(int level)
         {
-            _brightness = level;
+            brightness = level;
             Console.WriteLine($"[TechPro] Яркость: {level}%");
         }
 
         public string GetStatus()
         {
-            return _isOn ? $"ВКЛ (Яркость: {_brightness}%)" : "ВЫКЛ";
+            return isOn ? $"ВКЛ (Яркость: {_brightness}%)" : "ВЫКЛ";
         }
     }
+
 }
