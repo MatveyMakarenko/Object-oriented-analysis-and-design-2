3f213922e6594eb48054fe8b5a922fe5c27985d8
MatveyMakarenko <148697980+MatveyMakarenko@users.noreply.github.com>
2026-02-26 21:58:33 +0700

    Update EcoLock.cs

--- lab01/vs/WindowsFormsApp1/WindowsFormsApp1/Devices/EcoLock.cs
+++ lab01/vs/WindowsFormsApp1/WindowsFormsApp1/Devices/EcoLock.cs
@@ -4,23 +4,24 @@ namespace SmartHomeWithoutPattern.Devices
 {
     public class EcoLock
     {
-        private bool _locked;
+        private bool locked;
 
         public void Lock()
         {
-            _locked = true;
+            locked = true;
             Console.WriteLine("[EcoHome] Замок заблокирован");
         }
 
         public void Unlock()
         {
-            _locked = false;
+            locked = false;
             Console.WriteLine("[EcoHome] Замок разблокирован");
         }
 
         public bool IsLocked()
         {
-            return _locked;
+            return locked;
         }
     }
+
 }
